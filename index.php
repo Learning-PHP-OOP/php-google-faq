08a174d0782f689c4a5b314f748296b4b4f1c59f
valentina <valentinagiannone@gmail.com>
2019-12-10 00:09:41 +0100

    compile google page

--- index.php
+++ index.php
@@ -4,15 +4,45 @@
   <meta charset="utf-8">
   <title>Google Faq</title>
   <link rel="stylesheet" href="/style.css">
-
+  <!--font -->
+  <link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet">
  </head>
-
  <body>
-  
-  <?php
-  include "data.php";
-  include "helper.php";
-  ?>
+  <div class="container">
 
+  <!--header -->
+  <header>
+   <div class="log">
+    <a href="#"><img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt="Google"></a>
+    <a> Privacy e termini </a>
+   </div>
+   <div class="navbar">
+   <div class="nav_list">
+     <ul>
+       <li>Introduzione</li>
+       <li>Norme sulla privacy</li>
+       <li>Termini di servizio</li>
+       <li>Tecnologie</li>
+       <li id="opentext">Domande frequenti</li>
+     </ul>
+   </div>
+   <div class="account">
+     <p>Account Google</p>
+   </div>
+   </div>
+  </header>
+
+  <!--main-->
+  <main>
+   <div class="faq">
+    <?php
+     include "data.php";
+     include "helper.php";
+    ?>
+   </div>
+  </main>
+
+
+ </div>
 </body>
 </html>
